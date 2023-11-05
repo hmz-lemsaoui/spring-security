044347e3e10f68eb0400280417a95ac797dd24a6
Hamza Lemsaoui <hamzalemsaoui@gmail.com>
2023-11-05 22:08:40 +0100

    b1 changes

---
+++ src/main/java/com/spring/springsecurity/security/ApplicationSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.spring.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) ->
+                        authz.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
